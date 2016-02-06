cc44ff7f923b85c1fac9d5d6cb1f0895a8fd9503
paulnagle <paulnagle@gmail.com>
2016-02-06 14:51:30 +0000

    Add clear for meeting filter

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -37,10 +37,10 @@ angular.module('na_ireland.controllers', [])
 		$ionicLoading.hide();
 	});
 
-$scope.refreshSearch = function(){
-	$ionicScrollDelegate.$getByHandle('mainScroll').scrollTop();
-	console.log("BOOM");
-}
+	$scope.refreshSearch = function(){
+		$ionicScrollDelegate.$getByHandle('mainScroll').scrollTop();
+		console.log("BOOM");
+	};
 
 	$scope.openMapsLink = function(destLatitude, destLongitude){
 		window.open('http://maps.google.com/maps?daddr=' + destLatitude + ',' + destLongitude, '_system', 'location=yes');
@@ -48,7 +48,6 @@ $scope.refreshSearch = function(){
 	};
 
 	$scope.heading = $stateParams.countyName;
-
 })
 
 // Meeting Map view controller
@@ -64,32 +63,38 @@ $scope.refreshSearch = function(){
 
 // Cleantime calculator view controller
 .controller('CleantimeCtrl', function($scope, $stateParams) {
-	function humanise(total_days)
-	{
-	    var date_current = new Date();
-	    var utime_target = date_current.getTime() + total_days*86400*1000;
-	    var date_target = new Date(utime_target);
+	function humanise(total_days)	{
+		var date_current = new Date();
+		var utime_target = date_current.getTime() + total_days*86400*1000;
+		var date_target = new Date(utime_target);
 
-	    var diff_year  = parseInt(date_target.getUTCFullYear() - date_current.getUTCFullYear());
-	    var diff_month = parseInt(date_target.getUTCMonth() - date_current.getUTCMonth());
-	    var diff_day   = parseInt(date_target.getUTCDate() - date_current.getUTCDate());
+		var diff_year  = parseInt(date_target.getUTCFullYear() - date_current.getUTCFullYear());
+		var diff_month = parseInt(date_target.getUTCMonth() - date_current.getUTCMonth());
+		var diff_day   = parseInt(date_target.getUTCDate() - date_current.getUTCDate());
 
-	    var days_in_month = [31, (date_target.getUTCFullYear()%4?29:28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-	    var date_string = "";
-	    while(true)
-	    {
-	        date_string = "That's ";
-	        date_string += (diff_year>0?diff_year + " years ":"");
+		var days_in_month = [31, (date_target.getUTCFullYear()%4?29:28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+		var date_string = "";
 
-	        if(diff_month<0){diff_year -= 1; diff_month += 12; continue;}
-	        date_string += (diff_month>0?diff_month + " months ":"");
+		while(true) {
+			date_string = "That's ";
+			date_string += (diff_year>0?diff_year + " years ":"");
 
-	        if(diff_day<0){diff_month -= 1; diff_day += days_in_month[((11+date_target.getUTCMonth())%12)]; continue;}
-	        date_string += (diff_day>0?diff_day + " days ":"");
-	        break;
-	    }
-	    console.log(date_string);
-	    return date_string;
+			if (diff_month < 0) {
+				diff_year -= 1;
+				diff_month += 12;
+				continue;
+			}
+			date_string += (diff_month>0?diff_month + " months ":"");
+
+			if (diff_day < 0) {
+				diff_month -= 1;
+				diff_day += days_in_month[ ((11 + date_target.getUTCMonth())%12) ];
+				continue;
+			}
+			date_string += (diff_day>0?diff_day + " days ":"");
+			break;
+		}
+		return date_string;
 	}
 
 	var _MS_PER_DAY = 1000 * 60 * 60 * 24;
@@ -116,7 +121,11 @@ $scope.refreshSearch = function(){
 			$scope.datepickerObject.inputDate = val;
 			var result = dateDiffInDays(val, d);
 			var dateString = humanise(result);
-			$scope.results = "<h4> You are " + result + " days clean!<br><p>" + dateString + "</p>";
+			if (result > 0) {
+				$scope.results = "<br><h4> You are " + result + " days clean!<h4><br><p>" + dateString + "</p>";
+			} else {
+				$scope.results = "<br><h4> You are clean today!<h4><br><p>Keep coming back!</p>";
+			}
 	  }
 	};
 
